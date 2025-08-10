2c6db44941f85aa01148419d1402a3967ed2e2ec
Eugen3113 <eg31it@gmail.com>
2025-08-10 11:35:09 +0200

    first commit typescript

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+
+
+// Consultation
+import Sandwich from './consultation/Sandwich/Sandwich'
+
+import './App.css'
+
+function App() {
+  return (<div className='app'>My first TypeScript Component
+   <>
+
+   {/* consultation */}
+   {/* <TodoList /> */}
+    <Sandwich />
+    
+    
+    </>
+  
+  </div>)
+}
+
+export default App
